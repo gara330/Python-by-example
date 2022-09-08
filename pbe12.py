aeaa0fbcb81c50cd4b83b73604ec1025371fed58
Krishna Salgado <gara98330@gmail.com>
2022-09-08 14:33:07 -0500

    Iniciando el repositorio con el ajercicio 12 del libro

---
+++ pbe12.py
@@ -0,0 +1,27 @@
+def comparator__to_minor():
+    numb1 = int(input("Enter a number: \n"))
+    numb2 = int(input("Enter one more number: \n"))
+    if numb1 > numb2:
+        print("The number: ", numb2, "Is lower than: ", numb1)
+        pass
+    else:
+        print("The number: ", numb1, "Is lower than ", numb2)
+        pass
+    pass
+def comparator_to_mayor():
+    numb1 = int(input("Enter a number: \n"))
+    numb2 = int(input("Enter one more number: \n"))
+    
+    if numb1 < numb2:
+        print("The number: ", numb2, "Is mayor than: ", numb1)
+        pass
+    else:
+        print("The number: ", numb1, "Is mayor than: ", numb2)
+        pass
+
+def main():
+    comparator__to_minor()
+    comparator_to_mayor()
+    pass
+
+main()
