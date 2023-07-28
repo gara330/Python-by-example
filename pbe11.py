18bfae09f14e50e241693003622d177f7f623639
Krishna Salgado <gara98330@gmail.com>
2023-07-28 00:11:22 -0600

    Little refactor

--- pbe11.py
+++ pbe11.py
@@ -1,8 +1,8 @@
-def howManyTimes():
+def how_many_times():
     largeNumber = int(input("Enter a number over 100 \n"))
     smallNumber = int(input("Enter a number under 10 \n"))
     hTimes = largeNumber//smallNumber
     print(smallNumber,"goes in", largeNumber, hTimes,"times")
     pass   
     
-howManyTimes()
+how_many_times()
