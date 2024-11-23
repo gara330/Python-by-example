1bd42759800e26ce2ed37f263e088de1e35b11f7
Krishna Salgado <gara98330@gmail.com>
2024-11-22 19:09:31 -0600

    finishing 91

--- pbe91.py
+++ pbe91.py
@@ -1,6 +1,9 @@
 def repeat_array():
-    num_array = [1, 5, 5, 4, 6]
+    num_array = [1, 5, 5, 4, 4]
     print("Here is the array: \n", num_array)
     user_number = int(input("Enter a number from de array: "))
+    print("That number appears: ", num_array.count(user_number), " times")
 
     pass
+
+repeat_array()
